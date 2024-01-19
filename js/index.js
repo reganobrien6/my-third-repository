bdd0b3f20dfa211651355d227481db9428358e70
reganobrien6 <137224304+reganobrien6@users.noreply.github.com>
2024-01-18 22:43:47 -0600

    Jan 18 Hw 19
    Changes as of Jan 18

--- js/index.js
+++ js/index.js
@@ -1,1 +1,6 @@
 console.log("your index.js file is loaded correctly!");
+$('.paintingLink').on('click', scrollWin);
+function scrollWin() {
+    console.log("scrolling")
+    window.scrollTo(0, 1000);
+  } 
